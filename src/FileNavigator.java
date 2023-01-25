260641db04430365350e1df983f2e2e45d077875
Sytnyk Yurii <yoursit1980@gmail.com>
2023-01-25 22:55:11 +0200

    homework14

--- src/FileNavigator.java
+++ src/FileNavigator.java
@@ -1,83 +1,54 @@
+import java.awt.geom.IllegalPathStateException;
 import java.util.*;
 
 public class FileNavigator {
 
-    private String ROOT_DIR = "DIR";
-    private Map<String, List<FileData>> listMap;
-    private List<FileData> list;
+    private final String ROOT_DIR = "DIR";
+    private final Map<String, List<FileData>> listMap = new HashMap<>();
 
-    public FileNavigator() {
-        this.listMap = new HashMap<>();
-    }
+    public void add(FileData file) {
 
-    //    public void add(FileData file) {
-//        if (Objects.equals(file.getPath(), ROOT_DIR)) {
-//            if (!listMap.containsKey(file.getPath())) {
-//                list = new ArrayList<>();
-//                list.add(file);
-//                listMap.put(file.getPath(), list);
-//            } else {
-//                list = new ArrayList<>();
-//                list = listMap.get(file.getPath());
-//                list.add(file);
-//                listMap.put(listMap.get(file.getPath()).toString(), list);
-//            }
-//        }
-//    }
-    public void add(FileData fileData) {
-        List<FileData> fileDataList = listMap.get(fileData.getPath());
-        if (fileDataList != null) {
-            fileDataList.add(fileData);
-        } else {
-            System.out.println(" Incorrect file path");
+        if (file.getPath().indexOf(ROOT_DIR) != 0) {
+            String message = "\nAdded new file directory [" + file.getPath() + "]"
+                    + " is not root directory! [" + ROOT_DIR + "]";
+            throw new IllegalPathStateException(message);
         }
+        listMap.putIfAbsent(file.getPath(), new ArrayList<>());
+        listMap.get(file.getPath()).add(file);
     }
 
-    public void find(String path) {
-        if (listMap.containsKey(path)) {
-            System.out.println(" Finding by path :" + '\n' + listMap.get(path));
-        } else {
-            System.out.println(" File not found ;");
+    public List<FileData> find(String path) {
+        return listMap.get(path);
+    }
+
+    public List<FileData> findAll() {
+        List<FileData> files = new ArrayList<>();
+        for (List<FileData> list : listMap.values()) {
+            files.addAll(list);
         }
+        return files;
     }
 
-    public Map<String, List<FileData>> filterBySize(int size) {
-        Map<String, List<FileData>> lm = new HashMap<>();
-        for (List<FileData> fd : listMap.values()) {
-            for (FileData fileData : fd) {
-                if (fileData.getSize() < size) {
-                    lm.put(fileData.getPath(), fd);
-                }
+    public List<FileData> filterBySize(int size) {
+        List<FileData> files = new ArrayList<>();
+        for (FileData file : findAll()) {
+            if (file.getSize() <= size) {
+                files.add(file);
             }
         }
-        return lm;
+        return files;
     }
 
-    public void remove(String path) {
+    public String remove(String path) {
         listMap.remove(path);
+        return path;
     }
 
-//    public List<FileData> sortBySize() {
-//        List<FileData> list = new ArrayList<>();
-//        for (String key : listMap.keySet()) {
-//            list.addAll(listMap.get(key));
-//            list.sort(new Comparator<FileData>() {
-//                @Override
-//                public int compare(FileData o1, FileData o2) {
-//                   // return o1.getSize().compareTo(o2.getSize());
-//                }
-//            });
-//        }
-//        return list;
-//    }
-
-    public void addListMap(String ROOT_DIR) {
-        if (!listMap.containsKey(ROOT_DIR)) {
-            listMap.put(ROOT_DIR, new ArrayList<FileData>());
-        }
-    }
-
-    public Map<String, List<FileData>> getListMap() {
-        return listMap;
+    public List<FileData> sortBySize() {
+        List<FileData> fileDataList = findAll();
+        Collections.sort(fileDataList);
+        return fileDataList;
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ public class Main {
         fileNavigator.add(new FileData("file222.txt", 60, "DIR"));
         fileNavigator.add(new FileData("file333.txt", 70, "DIR"));
         fileNavigator.add(new FileData("file444.txt", 80, "DIR"));
-        fileNavigator.add(new FileData("file555.txt", 100, "DIRECT"));
+        fileNavigator.add(new FileData("file555.txt", 100, "DIR/dir"));
+        fileNavigator.add(new FileData("file666.txt", 20, "DIR"));
 
-        System.out.println(fileNavigator.filterBySize(70)+"\n");
-
-
-
-    }
-
+        System.out.println("Find: "+fileNavigator.find("DIR"));
+        System.out.println("Filter by size"+ fileNavigator.filterBySize(70));
+        System.out.println("Remove " + fileNavigator.remove("DIR/dir"));
+        System.out.println("Find: "+fileNavigator.find("DIR/dir"));
+        System.out.println("Sort by size"+ fileNavigator.sortBySize());
+       }
 }
